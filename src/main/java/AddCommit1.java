9a28415530112a80c83fc1c947598c2990f203e6
nisaa <nisa.ayhan05@gmail.com>
2026-03-12 09:55:35 +0100

    innit

--- src/main/java/AddCommit1.java
+++ src/main/java/AddCommit1.java
@@ -2,5 +2,6 @@ public class AddCommit1 {
 
     public static void main() {
         System.out.println("commit 1 ready");
+        System.out.println("update commit 1 completed.");
     }
 }
--- src/main/java/AddCommit2.java
+++ src/main/java/AddCommit2.java
@@ -1,5 +1,6 @@
 public class AddCommit2 {
     public static void main() {
         System.out.println("commit 2  completed");
+        System.out.println("update commit 2 completed.");
     }
 }
